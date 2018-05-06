bf49e8864091ba0226c24f5a2fe7131737e95cb9
morozzzz <36655453+morozzzz@users.noreply.github.com>
2018-05-06 13:58:49 +0300

    fix cross browser issues

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
+const spinner = document.querySelector('.loading-spinner');
 const menu = document.querySelector('.navigation');
-const activeMenuItemId = sessionStorage.activMenuItemId;
-const menuItemToSetActive = document.querySelector(`#${activeMenuItemId}`);
+const activeMenuItemId = sessionStorage['activMenuItemId'];
+const menuItemToSetActive = document.querySelector('#' + activeMenuItemId);
 const menuChildNodes = document.querySelector('.navigation').childNodes;
 const firstMenuItem = Object.keys(menuChildNodes)
-    .map(key => menuChildNodes[key])
-    .filter(node => node.nodeName === 'LI')
+    .map(function(key) { return menuChildNodes[key]; })
+    .filter(function(node) { return node.nodeName === 'LI'; })
     .shift()
     .querySelector('a');
 
-const getMenuItemId = (event) => {
+const getMenuItemId = function(event) {
     const menuItem = event.target;
     const currentMenuItemId = menuItem.id;
     sessionStorage.setItem('activMenuItemId', currentMenuItemId);
 };
+
+const deleteSpinner = function () { document.body.removeChild(spinner); };
 
 menu.addEventListener('click', getMenuItemId);
 
@@ -20,13 +23,15 @@ menuItemToSetActive
     ? menuItemToSetActive.classList.add('active')
     : firstMenuItem.classList.add('active');
 
-window.onbeforeunload = () => {
-    window.onunload = () => {
+window.onbeforeunload = function() {
+    window.onunload = function() {
         window.sessionStorage.isMySessionActive = 'false';
     };
     return undefined;
 };
 
-window.onload = () => {
+window.onload = function() {
     window.sessionStorage.isMySessionActive = 'true';
 };
+
+setTimeout(deleteSpinner, 2000);
